1b18de162d8eee712778dda33c0b351524510d1e
Llegado <wellingtonhubert54@gmail.com>
2026-01-11 13:05:14 +0800

    changes changes in resources and public folder

--- vetsystem/resources/views/register.blade.php
+++ vetsystem/resources/views/register.blade.php
@@ -1,66 +1,52 @@
 @extends('layout.base')
 
 @section('content')
-<div class="register-bg">
-    <div class="register-card">
+<div class="page-bg">
+    <div class="page-card">
         <h3>Register</h3>
         <p>Vet Clinic Registration</p>
 
         <form action="/register/create" method="POST">
             @csrf
 
-            <div class="row mb-3">
-                <div class="col-6 mb-3">
+            <div class="row g-3 mb-3">
+                <div class="col-6">
                     <label class="form-label">Last Name*</label>
                     <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}">
-                    @error('last_name')
-                    <span class="text-danger">{{ $message }}</span>
-                    @enderror
+                    @error('last_name') <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
-                <div class="col-6 mb-3">
+
+                <div class="col-6">
                     <label class="form-label">First Name*</label>
                     <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}">
-                    @error('first_name')
-                    <span class="text-danger">{{ $message }}</span>
-                    @enderror
+                    @error('first_name') <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
             </div>
 
-            <div class="row mb-3">
+            <div class="row g-3 mb-3">
                 <div class="col-6">
                     <label class="form-label">Age*</label>
                     <input type="number" class="form-control @error('age') is-invalid @enderror" name="age" value="{{ old('age') }}">
-                    @error('age')
-                    <span class="text-danger">{{ $message }}</span>
-                    @enderror
+                    @error('age') <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
+
                 <div class="col-6">
                     <label class="form-label">Contact Number*</label>
                     <input type="tel" class="form-control @error('contact_number') is-invalid @enderror" name="contact_number" value="{{ old('contact_number') }}">
-                    @error('contact_number')
-                    <span class="text-danger">{{ $message }}</span>
-                    @enderror
+                    @error('contact_number') <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
             </div>
 
-            <div class="mb-3">
-                <label class="form-label">Username*</label>
-                <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}">
-                @error('username')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
+            <label class="form-label">Username*</label>
+            <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}">
+            @error('username') <span class="text-danger">{{ $message }}</span> @enderror
 
-            <div class="mb-3">
-                <label class="form-label">Password*</label>
-                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
-                @error('password')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
+            <label class="form-label">Password*</label>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+            @error('password') <span class="text-danger">{{ $message }}</span> @enderror
 
-            <div class="d-grid">
-                <button class="btn btn-primary">Submit</button>
+            <div class="d-grid mt-3">
+                <button class="btn btn-primary">Register</button>
                 <a href="/login" class="btn btn-secondary">Back</a>
             </div>
         </form>
